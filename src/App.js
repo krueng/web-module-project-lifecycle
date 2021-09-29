8171d5267fe2aa7d4039cd99e8b49f08adb09c3e
Adam K <krueng@gmail.com>
2021-09-28 20:16:23 -0500

    completed strecthes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import './App.css';
 class App extends React.Component {
   state = {
     username: '',
-    userImg: [],
+    userImg: '',
     name: '',
     location: '',
     url: '',
-    id: ''
+    id: '',
+    followers: []
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/users/krueng')
       .then(resp => {
-        console.log(resp)
         this.setState({
           username: resp.data.login,
           userImg: resp.data.avatar_url,
@@ -29,11 +29,24 @@ class App extends React.Component {
         console.error('error')
       )
   }
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevState.username, this.state.username)
+    if (this.state.username !== prevState.username) {
+      axios.get(`https://api.github.com/users/${this.state.username}/followers`)
+        .then(resp => {
+          this.setState({
+            followers: resp.data
+          })
+        })
+        .catch(
+          console.error('error')
+        )
+    }
 
+  }
   handleSearch = (e) => {
     e.preventDefault();
     this.state.username.length >= 1 && //Ensure something was typed
-      // console.log(`${this.state.user}`);
       axios.get(`https://api.github.com/users/${this.state.username}`)
         .then(resp => {
           this.setState({
@@ -56,21 +69,27 @@ class App extends React.Component {
       username: e.target.value
 
     });
-    // console.log(`${e.target.value}`);
   }
 
   render() {
     return (
       <div className="App">
         <form>
-          <input value={this.state.username} onChange={this.handleInput} placeholder='search by username'/>
+          <input value={this.state.username} onChange={this.handleInput} placeholder='search by username' />
           <button onClick={this.handleSearch}>Search User</button>
         </form>
         <header className="App-header">
-          {/* this.state.userImg.map(img) */}
           <h1>{this.state.name}</h1>
-          <a href={this.state.url} target='_blank' rel='noreferrer'><img src={this.state.userImg} alt={this.state.name} /></a>
+          <a key={this.state.id} href={this.state.url} target='_blank' rel='noreferrer'><img src={this.state.userImg} alt={this.state.name} /></a>
           <p>{this.state.location}</p>
+
+          <h3> {`${this.state.name !== null ? this.state.name : ''} Has ${this.state.followers.length} Followers`} </h3>
+          {this.state.followers.map(follower =>
+            <>
+              <h4>{follower.login}</h4>
+              <a key={follower.id} href={follower.html_url} target='_blank' rel='noreferrer'><img width='50%' src={follower.avatar_url} alt={follower.login} /></a>
+            </>
+          )}
         </header>
       </div>
     );
